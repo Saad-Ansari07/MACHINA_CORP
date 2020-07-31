783935858cc1f52ba472dc6b7468512277dca474
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-07-31 18:21:19 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,29 @@
 import React from 'react';
 import './App.css';
 
-// <-- IMPORTING IMAGES && ICONS -->
+// <-- IMPORTING IMAGES -->
 import machine2 from './images/machine2.gif';
 import machine1 from './images/machine1.gif';
-import facebook from './images/facebook.ico';
-import home from './images/home.ico';
-import newacc from './images/newacc.ico';
-import settings from './images/settings.ico';
 
+// <-- IMPORTING ANIMATIONS -->
+import useWebAnimations, { shakeX, fadeInLeft, fadeInRight } from "@wellyshen/use-web-animations";
 
 function App() {
+    const { ref: x } = useWebAnimations({ ...shakeX });
+    const { ref: left } = useWebAnimations({ ...fadeInLeft });
+    const { ref: right } = useWebAnimations({ ...fadeInRight });
   return (
     <div className="container">
       <header className="header">
-      <h1 className="heading">MACHINA</h1>
-      <div className="menu">
+      <h1 className="heading" ref={x}>MACHINA</h1>
       <button className="btn">SIGN UP</button>
-      <div className="drop-menu">
-      <img className="icon1" src={facebook} alt={'logo'}></img>
-        <p  className="a6"> Sign up by FB </p>
-      <img className="icon2" src={home} alt={'logo'}></img>
-                  <p  className="a7">
-                          Your account
-                  </p>
-      <img className="icon3" src={settings} alt={'logo'}></img>           
-                  <p  className="a8">
-                          Account settings
-                  </p>
-      <img className="icon4" src={newacc} alt={'logo'}></img>         
-                  <p  className="a9">
-                          Make account
-                  </p>
-       </div>
-      </div>
-        <img className="mac" src={machine2} alt={'an automachine'}></img>
+        <img className="mac" src={machine2} alt={'an automachine'} ref={right}></img>
         <p className="par">Welcome to the industrial complex machina,<br></br> we will be honored to work with you.</p>
       </header>
       <div className="middle">
         <h2 className="t2">The future is now</h2>
         <p className="p2">Join us and realize your dream of automation in industry.</p>
-        <img className="mac2" src={machine1} alt={'an automachine'}></img>
+        <img className="mac2" src={machine1} alt={'an automachine'} ref={left}></img>
       </div>
       <div className="bottom">
         <p  className="a1">Details</p>
