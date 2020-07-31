4cf300b07c9942161648c1d2c1f9e37a42eef53c
Mohammad Saad Ansari <59654519+Saad-Ansari07@users.noreply.github.com>
2020-07-31 19:13:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 // <-- IMPORTING IMAGES -->
 import machine2 from './images/machine2.gif';
@@ -13,18 +15,22 @@ function App() {
     const { ref: y } = useWebAnimations({ ...shakeY });
     const { ref: left } = useWebAnimations({ ...fadeInLeft });
     const { ref: right } = useWebAnimations({ ...fadeInRight });
+  AOS.init({
+        offset: 200,
+        duration: 1000
+    });
   return (
     <div className="container">
       <header className="header">
       <h1 className="heading" ref={x}>MACHINA</h1>
       <button className="btn">SIGN UP</button>
         <img className="mac" src={machine2} alt={'an automachine'} ref={right}></img>
-        <p className="par">Welcome to the industrial complex machina,<br></br> we will be honored to work with you.</p>
+        <p className="par" ref={left}>Welcome to the industrial complex machina,<br></br> we will be honored to work with you.</p>
       </header>
-      <div className="middle">
+      <div className="middle" data-aos="fade-in">
         <h2 className="t2" ref={y}>The future is now</h2>
         <p className="p2">Join us and realize your dream of automation in industry.</p>
-        <img className="mac2" src={machine1} alt={'an automachine'} ref={left}></img>
+        <img className="mac2" src={machine1} alt={'an automachine'}></img>
       </div>
       <div className="bottom">
         <p  className="a1">Details</p>
